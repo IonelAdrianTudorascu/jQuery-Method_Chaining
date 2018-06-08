a85553869bafcb17c67a928eb884dabb4588ea94
Ionel Adrian Tudorascu <adrian_tudorascu@yahoo.ro>
2018-06-08 17:56:25 +0000

    added mouse enter and mouse leave functions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
- 
-})
+ 	//revoves claas makeRed, adds class makeBorder on mouseenter
+ 	$("button").mouseenter(function(){
+ 	 $(this).removeClass("makeRed").addClass("makeBorder");
+
+ 	});
+ 	//reverses above on mouseleave
+ 	$("button").mouseleave(function(){
+ 	 $("button").removeClass("makeBorder").addClass("makeRed");
+
+ 	});
+});
