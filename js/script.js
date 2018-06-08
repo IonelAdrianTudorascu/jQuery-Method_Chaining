4c173b4e0c6da5d29d5669baa6b1d26830a38841
Ionel Adrian Tudorascu <adrian_tudorascu@yahoo.ro>
2018-06-08 18:02:41 +0000

    added hie/show fadeIn/fadeOut functions

--- js/script.js
+++ js/script.js
@@ -14,4 +14,14 @@ $(document).ready(function() {
  	$("button").click(function(){
  	 $("p").slideToggle(2000);
  	});
+ 	
+ 	//hides/shows  paragraphs when either button is clicked
+ 	$("button").click(function(){
+ 	 $("p").hide(2000).show(2000);
+ 	});
+ 	
+ 		//fadeIn and fadoeOut on paragraphs when either button is clicked
+ 	$("button").click(function(){
+ 	 $("p").fadeIn().fadeOut();
+ 	});
 });
